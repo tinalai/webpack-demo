2f63e9bff7eeb70798421441ceb20cd3122e5100
Tina Lai <tina.lai@aofl.com>
2016-06-22 16:06:05 -0700

    integrates webpack-validator

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const merge = require('webpack-merge');
+const validate = require('webpack-validator');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -36,4 +36,4 @@ switch(process.env.npm_lifecycle_event) {
     config = merge(common, {});
 }
 
-module.exports = config;
+module.exports = validate(config);
