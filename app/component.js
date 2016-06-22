3410f482364422f5afbb0e3d71e281ecbd05815b
Tina Lai <tina.lai@aofl.com>
2016-06-22 16:33:15 -0700

    integrate webpack-dev-server

--- app/component.js
+++ app/component.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   var element = document.createElement('h1');
 
-  element.innerHTML = "Wassup world!";
+  element.innerHTML = "Hello world!";
 
   return element;
 };
