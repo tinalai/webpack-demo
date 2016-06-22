24fe3510efeb29aa43615a3a43b1e3f48e263ba2
Tina Lai <tina.lai@aofl.com>
2016-06-22 15:05:00 -0700

    set up webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // The latter form is more covenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
