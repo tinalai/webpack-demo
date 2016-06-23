e8a983218f8bb558d5b4f942a72b2964bf6686e7
Tina Lai <tina.lai@aofl.com>
2016-06-22 17:28:11 -0700

    adds polling for HMR on Windows, Ubuntu, and Vagrant

--- app/component.js
+++ app/component.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   var element = document.createElement('h1');
 
-  element.innerHTML = "the world is changing!";
+  element.innerHTML = "the world sees you!";
 
   return element;
 };
