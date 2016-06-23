ff7bed71e6d90d4b1b4cc3180aaeef6619b4e7d5
Tina Lai <tina.lai@aofl.com>
2016-06-23 16:05:35 -0700

    added CSS loading and initial CSS

--- libs/parts.js
+++ libs/parts.js
@@ -41,3 +41,17 @@ exports.devServer = function(options) {
     ]
   };
 };
+
+exports.setupCSS = function(paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: paths
+        }
+      ]
+    }
+  };
+};
