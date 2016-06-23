002a3c50f23efb539bb44f2b6417ed9c45456a5e
Tina Lai <tina.lai@aofl.com>
2016-06-22 17:25:05 -0700

    configures Hot Module Replacement

--- app/component.js
+++ app/component.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   var element = document.createElement('h1');
 
-  element.innerHTML = "Hello world!";
+  element.innerHTML = "the world is changing!";
 
   return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const validate = require('webpack-validator');
+const parts = require('./libs/parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -33,7 +34,14 @@ switch(process.env.npm_lifecycle_event) {
     config = merge(common, {});
     break;
   default:
-    config = merge(common, {});
+    config = merge(
+      common,
+      parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
 
 module.exports = validate(config);
